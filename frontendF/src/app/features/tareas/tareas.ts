cf62903ed468922161007a740185242d00765763
Op614246 <op614246@gmail.com>
2025-11-29 00:23:53 -0500

    estructura modular

---
+++ frontendF/src/app/features/tareas/tareas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas',
+  standalone: false,
+  templateUrl: './tareas.html',
+  styleUrl: './tareas.scss',
+})
+export class Tareas {
+
+}
